b4602039b762ec350c4c12b0b5d8f2dcf46e5a89
Naif <112791794+naifnadeem@users.noreply.github.com>
2024-10-17 11:53:09 +0500

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// vite.config.js
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: {
-    sourcemap: true, // Ensure this is set to true
+    sourcemap: mode !== 'production', // Enable sourcemap only in development
+    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit (optional)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
   },
-});
+}));
